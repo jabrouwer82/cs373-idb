3b0efd94724bc5938f85ef22e47b724fd7b5c56e
Parker Timmins <parkert@cs.utexas.edu>
2015-03-18 15:00:57 -0500

    Changed models to have explicit many-to-many relationships get Crimes, Attorneys, and Celebrities. Made all models inherit from JsonBuilder

--- models.py
+++ models.py
@@ -8,38 +8,31 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2:///celebsdb'
 db = SQLAlchemy(app)
 
-# Wrapper for a dictionary to build up JSON for each model
-class JSONMap:
-  def __init__(self, model):
-    self.json_map = dict()
-    self.model = model
+def get_ids(t):
+  return list(set(c.id for c in t))
 
-  def add(self, key, value):
-    self.json_map[key] = value
-    return self
-    
-  def add_scalars(self,*scalars):
-    for s in scalars:
-      self.json_map[s] = self.model.__dict__[s]
-    return self
-  
-  def add_for_charges(self, name, f):
-    self.json_map[name] = list(set(f(c) for c in self.model.charges.all()))
-    return self
-  def add_celebrities(self):
-    return self.add_for_charges('celebrities', lambda c:c.celebrity_id)
-  def add_crimes(self):
-    return self.add_for_charges('crimes', lambda c:c.crime_id)
-  def add_charges(self):
-    return self.add_for_charges('charges', lambda c:c.id)
-  def add_attorneys(self):
-    return self.add_for_charges('attorneys', lambda c:c.attorney_id)
+class JsonBuilder:
 
+  json_attr = ()
+  json_attr_func = ()
 
-class Celebrity(db.Model):
+  def to_json(self):
+    json_map = {}
+    json_map.update({p:getattr(self, p) for p in self.json_attr})
+    json_map.update({k:f(getattr(self,k)) for k,f in self.json_attr_func})
+    return json_map
+
+class Celebrity(db.Model, JsonBuilder):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(80), unique=True)
   description = db.Column(db.Text)
+
+  attorneys = db.relationship("Attorney", secondary='charge')
+  crimes = db.relationship("Crime", secondary='charge')
+  charges = db.relationship("Charge")
+
+  json_attr = ('id', 'name', 'description')
+  json_attr_func = [(k,get_ids) for k in ('attorneys', 'crimes', 'charges')]
 
   def __init__(self, name, description = None):
     self.name = name
@@ -48,17 +41,18 @@ class Celebrity(db.Model):
   def __repr__(self):
     return '(Celebrity %r)' % self.name
 
-  def to_json(self):
-    return JSONMap(self).add_scalars('id', 'name', 'description') \
-            .add_attorneys() \
-            .add_crimes() \
-            .add_charges().json_map
 
-
-class Attorney(db.Model):
+class Attorney(db.Model, JsonBuilder):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(80), unique=True)
   firm = db.Column(db.String(80), unique=True)
+
+  celebrities = db.relationship("Celebrity", secondary='charge')
+  crimes = db.relationship("Crime", secondary='charge')
+  charges = db.relationship("Charge")
+
+  json_attr = ('id', 'name', 'firm')
+  json_attr_func = [(k,get_ids) for k in ('celebrities', 'crimes', 'charges')]
 
   def __init__(self, name, firm = None):
     self.name = name
@@ -66,40 +60,41 @@ class Attorney(db.Model):
 
   def __repr__(self):
     return '(Attorney %r)' % self.name
-
-  def to_json(self):
-    return JSONMap(self) \
-            .add_scalars('id', 'name', 'firm') \
-            .add_celebrities() \
-            .add_crimes() \
-            .add_charges().json_map
       
-class Crime(db.Model):
+class Crime(db.Model, JsonBuilder):
   id = db.Column(db.Integer, primary_key=True)
   title = db.Column(db.String(80), unique=True)
+  celebrities = db.relationship("Celebrity", secondary='charge')
+  attorneys = db.relationship("Attorney", secondary='charge')
+  charges = db.relationship("Charge")
+
+  json_attr = ('id', 'title')
+  json_attr_func = [(k,get_ids) for k in ('celebrities', 'attorneys', 'charges')]
 
   def __init__(self, title):
     self.title = title 
+
   def __repr__(self):
     return '(Crime %r)' % self.title
 
-  def to_json(self):
-     return JSONMap(self).add_scalars('id', 'title') \
-            .add_attorneys() \
-            .add_celebrities() \
-            .add_charges().json_map
 
+# make this association table
+class Charge(db.Model, JsonBuilder):
 
-class Charge(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   date = db.Column(db.DateTime)
   location = db.Column(db.String(80), unique=True)
+
   celebrity_id = db.Column(db.Integer, db.ForeignKey('celebrity.id'))
-  celebrity = db.relationship('Celebrity', backref=db.backref('charges', lazy='dynamic'))
   attorney_id = db.Column(db.Integer, db.ForeignKey('attorney.id'))
-  attorney = db.relationship('Attorney', backref=db.backref('charges', lazy='dynamic'))
   crime_id = db.Column(db.Integer, db.ForeignKey('crime.id'))
-  crime = db.relationship('Crime', backref=db.backref('charges', lazy='dynamic'))
+  
+  crime = db.relationship('Crime')
+  attorney = db.relationship('Attorney')
+  celebrity = db.relationship('Celebrity')
+
+  json_attr = ('crime_id', 'attorney_id', 'crime_id', 'location')  
+  json_attr_func = [('date', lambda d : d.strftime("%m-%d-%Y"))]
 
   def __init__(self, celebrity, crime, attorney, date , location):
     self.celebrity = celebrity
@@ -108,12 +103,6 @@ class Charge(db.Model):
     self.date = date
     self.location = location
 
+
   def __repr__(self):
     return '(Charge %r %r %r)' % (self.crime, self.celebrity, self.attorney)
-
-  def to_json(self):
-    return JSONMap(self).add_scalars('celebrity_id', 'attorney_id', 'crime_id', 'location') \
-                        .add('date', self.date.strftime("%m-%d-%Y")) \
-                        .json_map
-
-
