0e4d42ea2536990f730a57cf0c4c8cdaf447c31b
Ian <i.daszkowski92@gmail.com>
2015-03-24 13:20:26 -0500

    Added templates for about us, celebrity, and splash pages.

--- views.py
+++ views.py
@@ -1,12 +1,21 @@
-from flask import render_template,Flask
+from flask import render_template, Flask
 from app import app
-
+from models import Crime, Celebrity, Charge
 
 
 @app.route('/')
 @app.route('/splash')
 def splash():
-  return render_template('splash.html')
+    return render_template('splash.html')
+
+@app.route('/about_us')
+def about_us():
+    return render_template('about_us.html')
+
+@app.route('/celebrities')
+def celebrities():
+    celebrity_list = Celebrity.query.all()
+    return render_template('celebrities.html', celebrities=celebrity_list)
      
 
 
