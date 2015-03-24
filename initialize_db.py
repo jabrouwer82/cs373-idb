5cf6fbbcb9a5400a5af6aa20a4aeeda293b45c0d
Jacob Brouwer <jabrouwer82@gmail.com>
2015-03-24 21:55:22 +0000

    Adds more data to be input.

--- initialize_db.py
+++ initialize_db.py
@@ -160,4 +160,44 @@ Snoop Dogg
 Burbank, California
 Broadus was first arrested Oct. 26 at Burbank Airport. Thompson said he had 39.14 grams of marijuana – some of it in individual canisters – at the time of this arrest. A search warrant served at his home in the Diamond Bar in November turned up a semi-automatic handgun,
 
+Earl Simmons (DMX, Dark Man X)
+Rapper, ...etc
+https://en.wikipedia.org/wiki/DMX_%28rapper%29
+http://www.imdb.com/name/nm0229422/
+@DMX
+12-18-1970
+7-2-199 When officers of the Fort Lee Police Department executed a search of his home in 1999, DMX promptly surrendered himself on weapons possession charges
+5-5-2000 DMX served a 15-day jail sentence in 2000 for possession of marijuana.
+5-22-2000 DMX served another jail sentence in 2001 for driving without a license and possession of marijuana. His appeal to reduce the sentence was denied; rather, he was charged with assault for throwing objects at prison guards. DMX entered rehab to treat his addiction to drugs in 2002
+6-26-2004 DMX was arrested in June 2004, at the John F. Kennedy International Airport, on charges of cocaine possession, criminal impersonation, criminal possession of a weapon, criminal mischief, menacing, and driving under the influence of drugs or alcohol, while claiming to be a federal agent and attempting to carjack a vehicle. He was given a conditional discharge on December 8, 2004, but pled guilty on October 25, 2005, to violating parole.
+11-18-2005 DMX was sentenced to 70 days in jail on November 18, 2005, for violating parole; the lateness charge added a 10-day extension to the original 60-day sentence. DMX was released early (for "good behavior") on December 30, 2005.
+12-30-2008 DMX pled guilty to charges of drug possession, theft, and animal cruelty, at a hearing on December 30, 2008; he was sentenced to ninety days in jail on January 31, 2009.
+5-22-2009 On May 22, 2009, DMX entered a plea agreement/change of plea, and pled guilty to attempted aggravated assault.
+7-27-2010 On July 27, 2010, DMX turned himself in to Los Angeles Metropolitan Court for a reckless driving charge he received in 2002. He was sentenced to serve ninety days in jail
+11-19-2010 DMX was arrested in Maricopa County, Arizona on November 19, 2010, on charges of violating his probation by consuming alcohol (at a performance). On December 20, 2010, DMX was moved to the Mental Health Unit of the Arizona State Prison, and released on July 18, 2011.
+8-24-2011 DMX was arrested on August 24, 2011, for speeding (recorded as 102 mph in a 65 mph zone), reckless driving, and driving with a suspended license. While DMX admitted to speeding, he claims he was driving 85 mph, not 102 mph as charged
+2-13-2013 DMX was arrested on February 13, 2013 in Spartanburg, South Carolina for driving without a drivers license
+7-26-2013 He was arrested again on July 26, 2013 in Greenville County, South Carolina and charged with driving under the influence of alcohol, as well as driving without a license.
+8-20-2013 DMX was arrested on August 20, 2013 in Greer, South Carolina during a traffic stop after a car he was a passenger in made an improper u-turn. He was arrested due to an outstanding warrant for driving under suspension. Four packages of marijuana were also found in the vehicle, and he along with the driver were cited for them
+11-4-2013 DMX was again arrested on November 4, 2013 by the Greenville-Spartanburg International Airport police near Greer, South Carolina after police, who were familiar with his prior arrests, noticed DMX behind the wheel of a vehicle at the terminal. DMX was booked on charges of driving under suspension, having an uninsured vehicle, and not having a licensed vehicle. He was subsequently released after spending three hours in jail
+
+Ramón Antonio Gerardo Estévez (Martin Sheen)
+Actor, activist, ... etc
+https://en.wikipedia.org/wiki/Martin_Sheen
+http://www.imdb.com/name/nm0000640
+@csheensdad
+8-3-1940
+4-1-2007 Sheen was arrested, with 38 other activists, for trespassing at the Nevada Test Site at a Nevada Desert Experience event protesting against the site
+5-16-1990 Actor Martin Sheen and three others were arrested Wednesday morning after they splashed what appeared to be blood on the front of the downtown Federal Building in the latest of a series of demonstrations there to protest U.S. policies in El Salvador.
+8-12-2000 Martin was one of 22 people arrested for crossing over a line established by the Air Force in an anti-militarization protest at California's Vandenberg Air Force base. He was charged with trespassing.
+
+
+George Clooney
+Actor, ... etc
+@clooney_clooney
+https://en.wikipedia.org/wiki/George_Clooney
+http://www.imdb.com/name/nm0000123/
+5-6-1961
+3-16-2012 Clooney was arrested Friday for civil disobedience after taking part in a protest outside of the Sudanese embassy in Washington, D.C.
+
 '''
