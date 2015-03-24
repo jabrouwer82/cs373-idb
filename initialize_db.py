65f8ff1b2cae22c06073b7dd33205c132a9f0a6e
Parker Timmins <parkert@cs.utexas.edu>
2015-03-24 20:23:18 +0000

    Made some real initialization data

--- initialize_db.py
+++ initialize_db.py
@@ -35,7 +35,113 @@ db.session.add_all(charges)
 db.session.commit()
 
 
+# Celebrities
+cBroadus = Celebrity(name='Calvin Cordozar Broadus Jr.', 
+			  description='Musician, etc...', 
+                          twitter_handle='@SnoopDogg',
+			  birthday=date(1971,10,20),
+                          wiki_url='http://en.wikipedia.org/wiki/Snoop_Dogg',
+                          imdb_url='http://www.imdb.com/name/nm0004879/',
+                          picture_url=None)
+
+cSheen = Celebrity(name='Charley Sheen', 
+			  description='Actor, etc...', 
+                          twitter_handle='@CharleySheen',
+			  birthday=date(1965,9,3),
+                          wiki_url='http://en.wikipedia.org/wiki/Charley_Sheen',
+                          imdb_url='http://www.imdb.com/name/nm0000221/',
+                          picture_url=None)
+
+rDowneyJr = Celebrity(name='Robert Downey, Jr.', 
+			  description='Actor, etc...', 
+                          twitter_handle='@RobertDowneyJr',
+			  birthday=date(1965,4,4),
+                          wiki_url='http://en.wikipedia.org/wiki/Robert_Downey_Jr',
+                          imdb_url='http://www.imdb.com/name/nm0000375/',
+                          picture_url=None)
+
+mMcconaughey = Celebrity(name='Matthew McConaughey', 
+			  description='Actor, etc...', 
+                          twitter_handle='@McConaughey',
+			  birthday=date(1969,11,4),
+                          wiki_url='http://en.wikipedia.org/wiki/Matthew_McConaughey',
+                          imdb_url='http://www.imdb.com/name/nm0000190/',
+                          picture_url=None)
+
+celebrities = [cBroadus, cSheen, rDowneyJr, mMcconaughey]
+db.session.add_all(celebrities)
+
+# Aliases
+snoopDogg = CelebrityAlias('Snoop Dogg', cBroadus)
+
+
+aliases = [snoopDogg]
 
 
 
+# Crimes
+possessionMarijuana = Crime('Possession of Marijuana', 'http://en.wikipedia.org/wiki/Drug_possession')
+possessionHeroine = Crime('Possession of Heroine', 'http://en.wikipedia.org/wiki/Drug_possession')
+possessionCocaine = Crime('Possession of Cocaine', 'http://en.wikipedia.org/wiki/Drug_possession')
+possessionParaphernalia = Crime('Possession of Drug Paraphernalia', 'http://en.wikipedia.org/wiki/Drug_paraphernalia')
+resistingArrest = Crime('Resisting Arrest', 'http://en.wikipedia.org/wiki/Resisting_arrest')
+possessionFirearm = Crime('Possession of Concealed Firearm', 'http://en.wikipedia.org/wiki/Weapon_possession')
 
+crimes = [possessionMarijuana, possessionParaphernalia, possessionHeroine, possessionCocaine, possessionFirearm, resistingArrest]
+
+
+# Charges
+charges = [
+
+  Charge(mMcconaughey, possessionMarijuana, date(1999, 10, 29), 'Austin, Texas',
+    description='Actor Matthew McConaughey was arrested early Monday during a disturbance at his home in which police said he was dancing naked and playing the bongo drums.',	
+    attorney=None, classification=None),
+
+  Charge(mMcconaughey, possessionParaphernalia, date(1999, 10, 29), 'Austin, Texas',
+    description='Actor Matthew McConaughey was arrested early Monday during a disturbance at his home in which police said he was dancing naked and playing the bongo drums.'),
+
+  Charge(mMcconaughey, resistingArrest, date(1999, 10, 29), 'Austin, Texas',
+    description='Actor Matthew McConaughey was arrested early Monday during a disturbance at his home in which police said he was dancing naked and playing the bongo drums.',
+    classification='Class A misdemeanor'),
+
+  Charge(rDowneyJr, possessionHeroine, date(1996, 6, 23), 'Malibu, Calinfornia',
+    description='On June 23 at 11:15 a.m., Malibu sheriffs pulled him over after his black 1996 Explorer was clocked going 70 mph along a 50-mph stretch of the Pacific Coast Highway. They found heroin, cocaine and crack—and the gun'),
+
+  Charge(rDowneyJr, possessionCocaine, date(1996, 6, 23), 'Malibu, Calinfornia',
+    description='On June 23 at 11:15 a.m., Malibu sheriffs pulled him over after his black 1996 Explorer was clocked going 70 mph along a 50-mph stretch of the Pacific Coast Highway. They found heroin, cocaine and crack—and the gun'),
+
+  Charge(rDowneyJr, possessionFirearm, date(1996, 6, 23), 'Malibu, Calinfornia',
+    description='On June 23 at 11:15 a.m., Malibu sheriffs pulled him over after his black 1996 Explorer was clocked going 70 mph along a 50-mph stretch of the Pacific Coast Highway. They found heroin, cocaine and crack—and the gun'),
+
+	
+]
+
+# Add everything to the database
+db.session.add_all(crimes)
+db.session.add_all(celebrities)
+db.session.add_all(aliases)
+db.session.add_all(charges)
+db.session.commit()
+
+'''
+
+Robert Downey Jr.
+6-23-1996
+Malibu, California
+under the influence of a controlled substance
+
+Charley Sheen
+Aspen, Colorado
+Felony menacing, Third-degree assault (m), criminal mischief (m)
+Christmas Day incident in which he allegedly held a knife to his wife's throat in their rented Aspen home
+12-25-2009
+Sheen was arrested in Aspen, Colo., on charges of domestic violence, including assault and menacing, against his wife, Brooke Mueller. She reportedly told police that he had held a knife to her throat and threatened to kill her. Three months later, he was charged with felony menacing, and assault and criminal mischief, both misdemeanors. (latimes)
+
+
+Snoop Dogg
+10-26-2006 sale and transportation of marijuana
+11-2006 felony charges of gun possession by a felon
+Burbank, California
+Broadus was first arrested Oct. 26 at Burbank Airport. Thompson said he had 39.14 grams of marijuana – some of it in individual canisters – at the time of this arrest. A search warrant served at his home in the Diamond Bar in November turned up a semi-automatic handgun,
+
+'''
