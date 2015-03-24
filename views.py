d1677c44287821a48e319e1fddc470b841a4e2b0
Ian <i.daszkowski92@gmail.com>
2015-03-24 23:46:18 +0000

    Added the templates for all the different pillars of content.

--- views.py
+++ views.py
@@ -8,14 +8,25 @@ from models import Crime, Celebrity, Charge
 def splash():
     return render_template('splash.html')
 
-@app.route('/about_us')
-def about_us():
-    return render_template('about_us.html')
-
 @app.route('/celebrities')
 def celebrities():
     celebrity_list = Celebrity.query.all()
     return render_template('celebrities.html', celebrities=celebrity_list)
+
+@app.route('/crimes')
+def crimes():
+	crime_list = Crime.query.all()
+	return render_template('crimes.html', crimes=crime_list)
+
+@app.route('/charges')
+def charges():
+	charge_list = Charge.query.all()
+	return render_template('charges.html', charges=charge_list)
+
+@app.route('/about_us')
+def about_us():
+    return render_template('about_us.html')
+
      
 
 
