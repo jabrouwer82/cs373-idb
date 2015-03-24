b5d3cf6b384aa093e5df96d397716302371160b4
Jacob Brouwer <jabrouwer82@gmail.com>
2015-03-24 16:39:57 +0000

    External dev server, port option.
    Adds structure for further command line args.

--- run.py
+++ run.py
@@ -1,11 +1,22 @@
-#!../flask3/bin/python
+# Usage: python3 run.py [(-p | --port) <port nunmber to use>]
+# Example: python3 run.py -p 5050
+# -p or --port to specify the port to run the server on (default 5000)
 
 from app import app
+import getopt
+import sys
 
 # load the url routing functions
-
 from views import *
 from api import *
 
+# CMD opts handling
+opts = getopt.getopt(sys.argv[1:], 'p:', ['port='])
+port = 5000
+for opt, arg in opts[0]:
+  if opt in ('-p', '--port'):
+    port = int(arg)
 
-app.run(debug=True)
+
+app.run(host='0.0.0.0', port=port,  debug=True)
+
