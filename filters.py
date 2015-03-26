294a4ef5f428b24c9ce8664be1102f7572c687d2
Ian <i.daszkowski92@gmail.com>
2015-03-26 23:41:10 +0000

    Configured new filter. Might not need it if we're getting rid of crime descriptions.

--- filters.py
+++ filters.py
@@ -17,3 +17,20 @@ def firstSentence(string):
     res = res[:138]
     res += '...'
   return res
+
+def entireFirstSentence(string):
+  res1 = re.search(r'.+<br />', string)
+  # Some ammount of avything followed by a break tag. (First paragraph)
+  res1 = res1.group(0)[:-6] if res1 else ''
+  res2 = re.search('[^.]+\.', string)
+  # Some ammount of not periods followde by a period. (First sentence)
+  res2 = res2.group(0) if res2 else ''
+
+  res = res1 if len(res1) > len(res2) else res2
+  # This is necessary because some descriptions only have a single paragraph
+  # and thus no break tag, and some names have a '.' in them (Robert Downey Jr.)
+  # thus truncating early. This picks the longest option as the most correct one
+  if len(res) > 138:
+    res = res[:138]
+    res += '...'
+  return res
