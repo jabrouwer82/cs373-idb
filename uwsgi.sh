f75a037f3f867374bf4bfb9e1b63fab1657064d1
Jacob Brouwer <jabrouwer82@gmail.com>
2015-03-26 11:03:54 +0000

    Typo in uwsgi.sh.

--- uwsgi.sh
+++ uwsgi.sh
@@ -2,7 +2,7 @@
 
 opt=$1
 if [ "$opt" = "start" ]; then
-  uwsgi --socket 127.0.0.1:$sock --wsgi-file run.py --master --processes 4 --threads 2 --stats 127.0.0.1:5050 --callable app --pidfile uwsgi.pid &
+  uwsgi --socket 127.0.0.1:5000 --wsgi-file run.py --master --processes 4 --threads 2 --stats 127.0.0.1:5050 --callable app --pidfile uwsgi.pid &
 elif [ "$opt" = "stop" ]; then
   pidfile="uwsgi.pid"
   pid=$(cat "$pidfile")
