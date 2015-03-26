49179fcf98e950befcef9a19dff411d69f04efff
Jacob Brouwer <jabrouwer82@gmail.com>
2015-03-26 04:47:07 +0000

    Redoes the string truncation on the celeb descs.
    More flexible, finds either paragraphs or sentences to cut off.
    Makes more assumptions about data. Expects four &nbsp;s at start of string.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from filters import *
 
 app = Flask(__name__)
 app.jinja_env.autoescape = False
+app.jinja_env.filters['firstSentence'] = firstSentence
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2:///celebsdb'
 app.config['tipfyext.jinja2'] = {
     'environment_args': {
