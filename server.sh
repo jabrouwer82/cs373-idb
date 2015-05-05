6ebdb61f9956960207d7bbcbc3c95c2c6e746e43
Jacob Brouwer <jabrouwer82@gmail.com>
2015-05-05 02:00:52 +0000

    Server.sh now works globally and without sudo.
    logs and pid files are now stored at /tmp/server/ so any user can now start and stop the server without having to find them.
    The sudo calls have been made explicite witin the script, so you will still need to input your password, but you don't need to run sudo ./server.sh

--- server.sh
+++ server.sh
@@ -1,28 +1,46 @@
 #! /bin/bash
-# Usage: sudo ./server.sh
-# MUST BE SUPER USER TO COMPLETE
+# Usage: ./server.sh [start | stop | restart]
+# User must have superuser privilages to run this script.
 
+base="/tmp/server/"
+apppid="$base""uwsgi.pid"
+applog="$base""app.log"
+testpid="$base""test_uwsgi.pid"
+testlog="$base""test.log"
 
-apppid="uwsgi.pid"
-applog="app.log"
-testpid="test_uwsgi.pid"
-testlog="test.log"
+if [ ! -f "$testpid" ]; then
+  sudo install -Dv /dev/null "$testpid"
+fi
+if [ ! -f "$apppid" ]; then
+  sudo install -Dv /dev/null "$apppid"
+fi
+if [ ! -f "$testlog" ]; then
+  sudo install -Dv /dev/null "$testlog"
+fi
+if [ ! -f "$applog" ]; then
+  sudo install -Dv /dev/null "$applog"
+fi
+
+sudo chmod a+w "$testlog"
+sudo chmod a+w "$applog"
+sudo chmod a+w "$testpid"
+sudo chmod a+w "$apppid"
 
 opt=$1
 if [ "$opt" = "start" ]; then
-  # both $apppid and $testpid must not exist
-  if [ ! -r "$apppid" ] && [ ! -r "$testpid" ]; then
+  # both $apppid and $testpid must not exist or must be empty
+  if [ ! -s "$apppid" ] && [ ! -s "$testpid" ]; then
     echo "Copying nginx config to /etc/nginx/sites-enabled/default"
-    cp nginx.conf /etc/nginx/sites-enabled/default
+    sudo cp nginx.conf /etc/nginx/sites-enabled/default
     echo "Checking is nginx is running"
     if [ -r "/run/nginx.pid" ]; then
       echo "Nginx is running."
       echo "Reloading nginx"
-      nginx -s reload
+      sudo nginx -s reload
     else
       echo "Nginx is not running."
       echo "Starting nginx."
-      nginx
+      sudo nginx
     fi
     echo "Starting prod uwsgi server on port 5000, logs located at $applog"
     nohup uwsgi --socket 0.0.0.0:5000 --wsgi-file run.py --master --processes 4 --threads 2 --stats 0.0.0.0:5001 --callable app --pidfile "$apppid" &> "$applog" &
@@ -36,11 +54,11 @@ elif [ "$opt" = "stop" ]; then
   if [ -r "$apppid" ] || [ -r "$testpid" ]; then
     pid=$(cat "$apppid")
     echo "Killing prod uwsgi server"
-    kill -s SIGINT "$pid"
+    sudo kill -s SIGINT "$pid"
     rm "$apppid"
     echo "Killing test uwsgi server"
     pid=$(cat "$testpid")
-    kill -s SIGINT "$pid"
+    sudo kill -s SIGINT "$pid"
     rm "$testpid"
     echo "Success, killed both prod and test uwsgi servers."
   else
