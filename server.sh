8bfe36502b862d33d188d59fe19e1457956e7900
Jacob Brouwer <jabrouwer82@gmail.com>
2015-05-06 04:11:34 +0000

    server.sh handles log files better now.

--- server.sh
+++ server.sh
@@ -3,33 +3,50 @@
 # User must have superuser privilages to run this script.
 
 base="/tmp/server/"
-apppid="$base""uwsgi.pid"
-applog="$base""app.log"
-testpid="$base""test_uwsgi.pid"
-testlog="$base""test.log"
+archive="archive/"
+apppidfile="uwsgi.pid"
+applogfile="app.log"
+testpidfile="test_uwsgi.pid"
+testlogfile="test.log"
+apppid="$base""$apppidfile"
+applog="$base""$applogfile"
+testpid="$base""$testpidfile"
+testlog="$base""$testlogfile"
 
-if [ ! -f "$testpid" ]; then
-  sudo install -Dv /dev/null "$testpid"
-fi
-if [ ! -f "$apppid" ]; then
-  sudo install -Dv /dev/null "$apppid"
-fi
-if [ ! -f "$testlog" ]; then
-  sudo install -Dv /dev/null "$testlog"
-fi
-if [ ! -f "$applog" ]; then
-  sudo install -Dv /dev/null "$applog"
-fi
-
-sudo chmod a+w "$testlog"
-sudo chmod a+w "$applog"
-sudo chmod a+w "$testpid"
-sudo chmod a+w "$apppid"
 
 opt=$1
 if [ "$opt" = "start" ]; then
   # both $apppid and $testpid must not exist or must be empty
-  if [ ! -s "$apppid" ] && [ ! -s "$testpid" ]; then
+    if [ ! -s "$apppid" ] && [ ! -s "$testpid" ]; then
+    if [ ! -d "$base""$archive" ]; then
+      sudo mkdir -p "$base""$archive"
+    fi
+    if [ ! -f "$testpid" ]; then
+      sudo touch "$testpid"
+    fi
+    if [ ! -f "$apppid" ]; then
+      sudo touch "$apppid"
+    fi
+    if [ ! -f "$testlog" ]; then
+      sudo touch "$testlog"
+    else
+      echo "Moving old log files to $base$archive"
+      sudo mv "$testlog" "$base""$archive"$(date +"%m-%d-%y-%T")"-$testlogfile"
+      sudo touch "$testlog"
+    fi
+    if [ ! -f "$applog" ]; then
+      sudo touch "$applog"
+    else
+      sudo mv "$applog" "$base""$archive"$(date +"%m-%d-%y-%T")"-$applogfile"
+      sudo touch "$applog"
+    fi
+    echo "Creating pid and log files in $base"
+
+  sudo chmod a+w "$testlog"
+  sudo chmod a+w "$applog"
+  sudo chmod a+w "$testpid"
+  sudo chmod a+w "$apppid"
+  
     echo "Copying nginx config to /etc/nginx/sites-enabled/default"
     sudo cp nginx.conf /etc/nginx/sites-enabled/default
     echo "Checking is nginx is running"
@@ -51,7 +68,7 @@ if [ "$opt" = "start" ]; then
     echo "Server already running, please run [sudo ./server.sh restart] to quit and restart the server"
   fi
 elif [ "$opt" = "stop" ]; then
-  if [ -r "$apppid" ] || [ -r "$testpid" ]; then
+  if [ -s "$apppid" ] || [ -s "$testpid" ]; then
     pid=$(cat "$apppid")
     echo "Killing prod uwsgi server"
     sudo kill -s SIGINT "$pid"
